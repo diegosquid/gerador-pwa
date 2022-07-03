0a27ff3a1c90f2f71251540fda0fce1f4eebd7e7
Diego Macedo <diego.macedo@vorax.cc>
2022-07-03 19:08:26 -0300

    track

--- util/track.js
+++ util/track.js
@@ -7,6 +7,7 @@ export default function track () {
 
   try {
     fetch (`https://ct.popcash.net/click?aid=250197&clickid=${click}&payout=0`)
+    fetch (`http://postback.advertizer.com/pb.php?clickid=${click}&amount=0&advertiser_id=diegodiasmacedo&key=2d059be08a1df41e6dbe3efdf0219fe5`)
   } catch (e) {
     console.log(e)
   }
