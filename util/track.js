993898d5742306d259c240b64c3583cafc73340c
Diego Macedo <diego.macedo@vorax.cc>
2022-07-04 08:14:17 -0300

    track

--- util/track.js
+++ util/track.js
@@ -1,9 +1,5 @@
 export default function track () {
-  const url = new URL(window.location.href)
-  const utmSource = url.searchParams.get('utm_source')
-  const utmCampaign = url.searchParams.get('utm_campaign')
-  const utmMedium = url.searchParams.get('utm_medium')
-  const click = url.searchParams.get('click')
+  const click = window.localStorage.getItem('click')
 
   try {
     fetch (`https://ct.popcash.net/click?aid=250197&clickid=${click}&payout=0`)
