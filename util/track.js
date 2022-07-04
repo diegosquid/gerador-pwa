237c53188c0bd2ec9c63b11a91cf6a6f81da2f26
Diego Macedo <diego.macedo@vorax.cc>
2022-07-04 12:59:48 -0300

    fix track

--- util/track.js
+++ util/track.js
@@ -8,7 +8,7 @@ export default function track () {
   }
 
   try {
-    fetch (`http://postback.advertizer.com/pb.php?clickid=${click}&amount=0&advertiser_id=diegodiasmacedo&key=2d059be08a1df41e6dbe3efdf0219fe5`)
+    fetch (`https://postback.advertizer.com/pb.php?clickid=${click}&amount=0&advertiser_id=diegodiasmacedo&key=2d059be08a1df41e6dbe3efdf0219fe5`)
   } catch (e) {
     console.log(e)
   }
