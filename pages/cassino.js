f67df4d7651699e7d948efa723075c33ab08e40c
Diego Dias de Macedo <dmacedo@mcfadyen.com>
2024-02-16 20:53:57 -0300

    fix

--- pages/cassino.js
+++ pages/cassino.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Container from "@material-ui/core/Container"
 import Button from '@material-ui/core/Button'
-
+import { Typography } from "@material-ui/core"
 export default function Cassino () {
   const openCassino = () => {
     try {
@@ -16,7 +16,7 @@ export default function Cassino () {
   };
 
   return (
-    <div className={styles.casino}>
+    <div className={styles.casino} style={{marginBottom: '5rem'}}>
       <Head>
         <title>Cassino Online: Emoção de Jogar em Casa</title>
         <link rel="icon" href="/favicon.ico" />
@@ -32,6 +32,20 @@ export default function Cassino () {
           REGISTRE-SE AGORA
         </Button>
 
+      </Container>
+
+      <Container maxWidth="sm" className={styles.main}>
+        <Typography variant="h4" component="h2" style={{color: 'black', fontSize: 22, textTransform: "uppercase"}}> Uma Experiência de Jogo Incomparável no Fezbet Cassino
+        </Typography>
+        <Typography variant="p" component="p" style={{color: "black", marginTop: "1rem"}}>
+          O Fezbet Cassino destaca-se como uma joia no mundo dos jogos online, estabelecendo-se rapidamente como uma plataforma de topo para entusiastas de cassino de todos os níveis. Com uma impressionante variedade de jogos, que vão desde os clássicos caça-níqueis até jogos de mesa sofisticados, como poker, blackjack, e roleta, o Fezbet oferece uma experiência de jogo incomparável. Além disso, sua interface amigável e gráficos de alta qualidade asseguram uma imersão total no jogo, fazendo com que cada visita seja uma nova aventura. A dedicação do Fezbet em fornecer um ambiente de jogo seguro e justo, juntamente com seu compromisso com a excelência, solidifica sua posição como líder de mercado.
+        </Typography>
+        <Typography variant="h4" component="h2" style={{color: 'black', fontSize: 22, textTransform: "uppercase", marginTop: '1rem'}}>Bônus Generosos e Suporte Excepcional
+        </Typography>
+        <Typography variant="p" component="p" style={{color: "black", marginTop: "1rem"}}>  
+          Além da vasta seleção de jogos, o Fezbet Cassino eleva a experiência do usuário com bônus generosos e promoções regulares, projetadas para aumentar as chances de ganho dos jogadores e proporcionar ainda mais emoção. A equipe de suporte ao cliente do Fezbet é outra faceta que merece destaque; disponível 24/7, ela garante que qualquer dúvida ou problema seja resolvido com rapidez e eficiência. Esta combinação de um catálogo de jogos de primeira linha, atendimento ao cliente excepcional, e um ambiente de jogo seguro e acolhedor faz do Fezbet Cassino a escolha ideal para quem busca não apenas entretenimento, mas uma experiência de jogo de qualidade superior.
+        </Typography>
+        <br />
       </Container>
 
     </div>
